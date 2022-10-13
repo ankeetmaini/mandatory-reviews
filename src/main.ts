fef557313045a6f355c998c166a5d05c624db5bf
ajaychoudhary-hotstar <ajay.choudhary@hotstar.com>
2022-10-13 16:39:52 +0530

    Testing with fetch

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import {Octokit} from '@octokit/core'
 import * as All from '@octokit/types'
 
 type reviewsResponse =
@@ -11,24 +10,25 @@ async function run(): Promise<void> {
     const usernames: string = core.getInput('usernames')
     const group = usernames.split(',')
     const count = Number(core.getInput('count'))
-
-    const octokit = new Octokit({auth: process.env['GITHUB_TOKEN']})
-    console.log('token', process.env['GITHUB_TOKEN'])
     const [owner, repo] = process.env.GITHUB_REPOSITORY!.split('/')
     console.log(owner, repo)
     const pull_number = github.context.issue.number
     console.log(pull_number)
-    const res: reviewsResponse = await octokit.request(
-      'GET /repos/{owner}/{repo}/pulls/{pull_number}/reviews',
-      {
-        owner: owner,
-        repo: repo,
-        pull_number: pull_number
-      }
-    )
-    console.log(res.data)
-    const reviews = res.data
-      .map(d => {
+    var myHeaders = new Headers();
+    myHeaders.append("Accept", "application/vnd.github.v3+json");
+    myHeaders.append("Authorization", process.env['GITHUB_TOKEN'] as string);
+
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow'
+    };
+
+  fetch(`https://api.github.com/repos/${owner}/${repo}/pulls/${pull_number}/reviews`, requestOptions as any)
+    .then(response => response.json())
+    .then(res => {
+      const reviews = res.data!
+      .map((d: { user: { login: any }; state: any }) => {
         const login = d?.user?.login
         const state = d?.state
 
@@ -38,7 +38,9 @@ async function run(): Promise<void> {
         return
       })
       .filter(Boolean)
-    if (reviews.length < count) core.setFailed('Mandatory review check failed')
+    if (reviews.length < count) core.setFailed('Mandatory review check failed')})
+    .catch(error => console.log('error', error));
+    
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
